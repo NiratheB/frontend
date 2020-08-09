a508111651fbfbe2c5f712c257c2c62562d4cef4
Behtarin Ferdousi <behtarin.ferdousi@tum.de>
2020-08-09 19:05:37 +0200

    Add service to receive models

---
+++ src/app/model-list/model-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Model} from '../model';
+import {ModelService} from '../model.service';
+
+@Component({
+  selector: 'app-model-list',
+  templateUrl: './model-list.component.html',
+  styleUrls: ['./model-list.component.css']
+})
+export class ModelListComponent implements OnInit {
+  models: Model;
+  constructor(private modelservice: ModelService) { }
+
+  getModels(): void {
+    this.modelservice.getModels().subscribe(models => this.models = models);
+    console.log(this.models);
+  }
+  ngOnInit(): void {
+    this.getModels();
+  }
+
+}
